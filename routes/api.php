55e2d44747acc1f6bd35b18a1d792a07ef9225c3
krisna <kr15.last933@gmail.com>
2024-12-25 23:01:31 +0700

    Menambahkan UpdateUserController untuk API PUT /api/users/{id}

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\User\CreateUserController;
 use App\Http\Controllers\User\GetUsersController;
+use App\Http\Controllers\User\UpdateUserController;
 
 // Routing untuk membuat pengguna baru
 Route::post('/users', CreateUserController::class);
@@ -9,3 +10,5 @@ Route::post('/users', CreateUserController::class);
 // Routing untuk mengambil daftar pengguna
 Route::get('/users', GetUsersController::class);
 
+// Routing untuk merubah daftar pengguna
+Route::put('/users/{id}', UpdateUserController::class);
